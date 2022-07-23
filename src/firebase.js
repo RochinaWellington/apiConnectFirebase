3b0be801131fc584ed319c1e49d90bd00a7551a2
RochinaWellington <israelrochinagithub@gmail.com>
2022-07-23 17:25:10 -0500

    feat: data of Mario

--- src/firebase.js
+++ src/firebase.js
@@ -13,14 +13,15 @@ const firebaseConfig = {
   projectId: process.env.projectId,
   storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  appId: process.env.appId,
+  measurementId:process.env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export const getContacts = async (req, res) => {
-  const citiesCol = collection(db, 'contacts');
+  const citiesCol = collection(db, 'products');
   const citySnapshot = await getDocs(citiesCol);
   const cityList = citySnapshot.docs.map(doc => doc.data());
   res.json(cityList)
