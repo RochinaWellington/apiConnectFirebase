00b9d8467f14bbe4034455d4093f134326748b1b
RochinaWellington <israelrochinagithub@gmail.com>
2022-07-23 15:01:32 -0500

    feat: añdiendo coigo

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-const {initializeApp,applicationDefault}=require('firebase-admin/app')
-const{getFirestore}=require('firebase-admin/firestore')
-initializeApp({
-    credential:applicationDefault()
- })
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmN7eC2wj-m-AW994XKXpV8Iakcconnww",
+  authDomain: "fir-prueba-47f46.firebaseapp.com",
+  projectId: "fir-prueba-47f46",
+  storageBucket: "fir-prueba-47f46.appspot.com",
+  messagingSenderId: "552025277321",
+  appId: "1:552025277321:web:33f52bfcd302f0183bd758"
+};
 
- const db=getFirestore()
- module.exports={
-   db,
- }
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
